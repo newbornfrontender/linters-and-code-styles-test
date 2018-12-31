770cab7087daadc7fc0fdd6660ffac735e9f010b
Infant Frontender <newbornfrontender@gmail.com>
2018-12-31 21:03:52 +0300

    Добавил тестовый js файл

---
+++ index.js
@@ -0,0 +1,12 @@
+import template from './index.html';
+
+class MyApp extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+customElements.define('my-app', MyApp);
